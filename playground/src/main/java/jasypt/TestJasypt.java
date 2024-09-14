b9fc1dc2d1a2f77912912a927ca16955c22b6361
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-14 21:57:07 +0530

    No spring.config.import property has been defined Has been resolved

---
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -0,0 +1,16 @@
+package jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setPassword(System.getenv("JASYPT_PASSWORD"));
+        standardPBEStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = standardPBEStringEncryptor.encrypt(System.getenv("SPRING_CLOUD_PASSWORD"));
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
+    }
+}
