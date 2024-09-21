a365258098b47c125888434e857fa7954ea3c84b
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-21 21:14:28 +0530

    Added a service layer which holds all the query methods that can be called in the rest controller

---
+++ elastic-query-service/src/main/java/com/microservices/api/elastic/query/service/business/ElasticQueryService.java
@@ -0,0 +1,12 @@
+package com.microservices.api.elastic.query.service.business;
+
+import com.microservices.api.elastic.query.service.model.ElasticQueryServiceResponseModel;
+
+import java.util.List;
+
+public interface ElasticQueryService {
+
+    ElasticQueryServiceResponseModel getDocumentById(String id);
+    List<ElasticQueryServiceResponseModel> getDocumentByText(String text);
+    List<ElasticQueryServiceResponseModel> getAllDocuments();
+}
