303d3843860145517db9de5eb73fa430a9846a35
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-09-25 18:49:02 +0530

    Initiated Keycloack authentication implementation in elastic-query-service and added all the required classes to fetch configs from remote/local keyclock server

---
+++ elastic-query-service/src/main/java/com/microservices/api/elastic/query/service/security/TwitterQueryUser.java
@@ -0,0 +1,64 @@
+package com.microservices.api.elastic.query.service.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import static com.microservices.api.elastic.query.service.Constants.NA;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Builder
+@Getter
+public class TwitterQueryUser implements UserDetails {
+
+    private String username;
+
+    public void setPermissions(Map<String, PermissionType> permissions) {
+        this.permissions = permissions;
+    }
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    private Map<String, PermissionType> permissions;
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return NA;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
